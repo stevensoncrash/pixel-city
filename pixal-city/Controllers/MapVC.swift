9cd40aa4bd77fca939cb93cd5df1be58fe1d1f31
Stephen Reyes <sjreyes1@yahoo.com>
2018-07-18 18:17:20 -0700

    Adding UILabel for Pull Up View / Adding UICollectionView Programmatically

--- pixal-city/Controllers/MapVC.swift
+++ pixal-city/Controllers/MapVC.swift
@@ -14,20 +14,40 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     
     //outlets
     
+   
+
+    @IBOutlet weak var pullUpView: UIView!
+    @IBOutlet weak var pullUpHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
     
-
-
+    var screenSize = UIScreen.main.bounds
+    
+    var spinner : UIActivityIndicatorView?
+    var progressLbl : UILabel?
+    var flowLayout = UICollectionViewFlowLayout()
+    var collectionView: UICollectionView?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         locationManager.delegate = self
         configureLocationServices()
         addDoubleTap()
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        collectionView?.backgroundColor = #colorLiteral(red: 0.9771530032, green: 0.7062081099, blue: 0.1748393774, alpha: 1)
+        
+        
+        pullUpView.addSubview(collectionView!)
+        
     }
     
     func addDoubleTap(){
@@ -35,6 +55,56 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         doubleTap.numberOfTapsRequired = 2
         doubleTap.delegate = self
         mapView.addGestureRecognizer(doubleTap)
+        
+    }
+    
+    func addSwipe(){
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    func animateViewUp(){
+        pullUpHeightConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+             self.view.layoutIfNeeded()
+        }
+    }
+    @objc func animateViewDown(){
+        pullUpHeightConstraint.constant = 0
+        UIView.animate(withDuration:  0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    func addSpinner(){
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.activityIndicatorViewStyle = .whiteLarge
+        spinner?.color = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        spinner?.startAnimating()
+        collectionView?.addSubview(spinner!)
+
+    }
+    
+    func removeSpinner() {
+        if spinner != nil {
+            spinner?.removeFromSuperview()
+        }
+    }
+    
+    func addProgressLbl(){
+        progressLbl = UILabel()
+        progressLbl?.frame = CGRect(x: (screenSize.width / 2) - 120 , y: 175, width: 240, height: 40)
+        progressLbl?.font = UIFont(name: "Avenir Next" , size: 18)
+        progressLbl?.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        progressLbl?.textAlignment = .center
+        collectionView?.addSubview(progressLbl!)
+    }
+    
+    func removeProgressLbl() {
+        if progressLbl != nil {
+            spinner?.removeFromSuperview()
+        }
     }
     
     //actions
@@ -65,6 +135,15 @@ extension MapVC: MKMapViewDelegate{
     }
     @objc func dropPin(sender: UITapGestureRecognizer){
         removePin()
+        removeSpinner()
+        removeProgressLbl()
+        
+        animateViewUp()
+        addSwipe()
+        addSpinner()
+        addProgressLbl()
+      
+        
         
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
@@ -94,6 +173,19 @@ extension MapVC: CLLocationManagerDelegate{
     }
 }
 
+extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
+        return cell!
+    }
+}
 
 
 
---
+++ pixal-city/Views/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  pixal-city
+//
+//  Created by Stephen Reyes on 7/18/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init coder has not been implemented")
+    }
+    
+}
